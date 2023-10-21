7d48573d3039e1e2b81d97164040c157da64a022
unknown <Breke64@gmail.com>
2023-10-20 23:25:13 -0400

    began working on homework assignment have cards loaded in order still yet to finish functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,62 +1,151 @@
-var currentTime = dayjs();
-var $currentTime = $('p.currentDay');   //will be used to compare each row.
-var $row = $('div.row');   //class will change based on current time then will trigger respective css styles.
+const currentTime = dayjs().format(' h:mm:ss, dddd MMMM DD, YYYY');
+console.log(currentTime); // will be used to compare each block with.
+var $currentTime = $('p.currentDay');   
+var $cards = $('div.row');   //class will change based on current time then will trigger respective css styles.
 let $text = $('textarea.description');  //used for storing input into local storage.
-TimeHour = document.getElementById(`hour-{i}`); //most likely not targeting anything atm.
+var $hour = $('div.hour');
+$currentTime.text(currentTime);
+
+let scheduleCards = $('.container-lg');
+const localStorage = $('px-5');
+const saveTask = $('i.fas');
 
 
-$currentTime.text(dayjs());
+
+for(var i = 9; i < 13; i++){
+let newBlockAm =`
+<div id="hour-9  root" class="row time-block past">
+<div class="col-2 col-md-1 hour text-center py-3">${i}AM</div>
+<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  <i class="fas fa-save" aria-hidden="true"></i>
+</button>
+</div>`;
+scheduleCards.append(newBlockAm);
+$hour.text(i);
+console.log($hour); //think think think
+};
+
+for(var i = 1; i < 6; i++){
+  let newBlockPm =`
+  <div id="hour-9  root" class="row time-block past">
+  <div class="col-2 col-md-1 hour text-center py-3">${i}PM</div>
+  <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+    <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
+  </div>`;
+  scheduleCards.append(newBlockPm);
+  $hour.text(i);
+  $cards.toggleClass('past');
+  };
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+saveTask.click(function(){
+     var value = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // text.innerHTML = localStorage.getItem($text);
 // $text.textContent = text;
 // let val_serialized = JSON.stringify(val);
-$('i.fas').click(function(){
-  localStorage.setItem('todo', JSON.stringify($text));
-  textL = JSON.parse(localStorage.getItem('text'));
+// saveTask.click(function(){
+//    var value = $(this).siblings('.px-5').val();
+//     var time = $(this).parent().attr('id'); // will be a changing value based on time
+// localStorage.setItem(time,value);
+// });
+
+
+
+
+// want to change time text with each  iteritation in hour div
+
+
+
+
+
+
+
+
+
+
+// var  root = $('#root');
+
+// var divContainer = $('<div>');
+// divContainer.addClass('row');
+
+// var divHour = $('<div>');
+// divHour.addClass('hour');
+// divHour.text('10AM');
+
+//  var textL = $('<textarea>');
+//  textL.addClass('description');
+
+//  var btnIcon = $('<button>');
+//  btnIcon.addClass('btn');
+
+//  var saveT = $('<i>');
+//  saveT.addClass('fa-save');
+
+// root.append(divHour);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // localStorage.setItem('todo', JSON.stringify($text));
+  // var textL = JSON.parse(localStorage.getItem('todo'));
   // localStorage.setItem(textarea,'textarea');
   // alert( event.currentTarget === this );
-  console.log(localStorage);
-});
-// localStorage.setItem('data', JSON.stringify(data));
-
-var currentClass = 'past';
-const hourName = '9am';
-var hourData = '';
-
-
-
-
-  for (var i = 9; i > 17; i++){
-    var cards = $(`
-     <div id="hour-${i}" class="row time-block ${currentClass}">
-    <div class="col-2 col-md-1 hour text-center py-3">${hourData}</div>
-    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-      <i class="fas fa-save" aria-hidden="true"></i>
-    </button>
-  </div>
-  `);
-  document.body.append($row);
-  }
-
-
-
-
-// function getScheduleTime(projectTime) {
-//   var $row = $('.row').first();
-//   var $time = $row.find('.hour');
-//   var timeString = $time.text();
-//   var time = dayjs(timeString, 'h:mm A');
-
-//   if (dayjs().isSame(time, 'minute')) {
-//     $row.addClass('present');
-//   } else if (dayjs().isBefore(time, 'minute')) {
-//     $row.addClass('future');
-//   } else {
-//     $row.addClass('past');
-//   }
-// }
+  // console.log(textL);
+  // localStorage.setItem('');   -for when i get the cards done
 
 
 
@@ -66,33 +155,17 @@ var hourData = '';
 
 
 
-// things changing 1 hour, 2 class of time, 
-// const newBlock = $(`
-// <div id="hour-${i}" class="row ${currentClass}">
-// <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-// <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-// <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-//   <i class="fas fa-save" aria-hidden="true"></i>
-// </button>
-// </div>`);
 
 
 
 
 
 
-// for (var i = 9; i > 17; i ++){
+
+
   
-// }
-
-// currentClass = dayjs;
-
-// console.log(newBlock);
 
 
-
-
-// localStorage.setItem('');   -for when i get the cards done
 
 
 
@@ -117,5 +190,4 @@ var hourData = '';
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  

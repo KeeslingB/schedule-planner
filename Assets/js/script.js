645889388c8a89d32a6b1b775a52cc5a704a1e15
unknown <Breke64@gmail.com>
2023-10-21 22:42:48 -0400

    continued some work on homwork hopfully only have local storage left not working now for some reason

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,117 +1,79 @@
-const currentTime = dayjs().format(' h:mm:ss, dddd MMMM DD, YYYY');
+var currentTime = dayjs().format(' h:mm:ss, dddd MMMM DD, YYYY');
 console.log(currentTime); // will be used to compare each block with.
-var $currentTime = $('p.currentDay');   
-var $cards = $('div.row');   //class will change based on current time then will trigger respective css styles.
+var $currentTime = $('p.currentDay');
+var $cards = $('div.row');   //class will change based on current time then will trigger respective css styles. //root?
 let $text = $('textarea.description');  //used for storing input into local storage.
+saveBtn = document.getElementsByClassName('fa-save');
 var $hour = $('div.hour');
 $currentTime.text(currentTime);
 
 let scheduleCards = $('.container-lg');
 var localStorage = $('px-5');
-var saveTask = $('i.fas');
+var saveTask = document.getElementsByClassName('i.fas');
 
-timeclasses = '';
-
-for(var i = 9; i < 13; i++){
-let newBlockAm =`
-<div id="hour-9  root" class="row time-block ">
+for (var i = 9; i < 13; i++) {
+  if (i < currentTime) {
+    am = 'past';
+  }
+  if (i == currentTime) {
+    am = 'present';
+  } else (am = 'future');
+  let newBlockAm = `
+<div id="hour-${i}am  root" class="row time-block ${am}">
 <div class="col-2 col-md-1 hour text-center py-3">${i}AM</div>
 <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
 <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  <i class="fas fa-save" aria-hidden="true"></i>
+  <i class="fas fa-save" aria-hidden="false"></i>
 </button>
 </div>`;
-scheduleCards.append(newBlockAm);
-$hour.text(i);
-// console.log(i);
+  $hour.text(i);
+  scheduleCards.append(newBlockAm);
+  // console.log(newBlockAm);
 };
 
-
-
-for(var i = 1; i < 6; i++){
-  let newBlockPm =`
-  <div id="hour-9  root" class="row time-block ">
+for (var i = 1; i < 6; i++) {
+  if (i < currentTime) {
+    pm = 'past';
+  }
+  if (i == currentTime) {
+    pm = 'present';
+  } else (pm = 'future');
+  let newBlockPm = `
+  <div id="hour-${i}pm  root" class="row time-block ${pm}">
   <div class="col-2 col-md-1 hour text-center py-3">${i}PM</div>
   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    <i class="fas fa-save" aria-hidden="true"></i>
+    <i class="fas fa-save" aria-hidden="false"></i>
   </button>
   </div>`;
   scheduleCards.append(newBlockPm);
   $hour.text(i);
-  $cards.addClass('present');
-  };
-  
+  // console.log(newBlockPm);
+};
 
-
-
-// still figuiring out how to compare numbers to time inorder to add classes for css.
-
-
-
-
-
-
-
-
-
-
-
-
-
-saveTask.click(function(){
-     var value = $(this).siblings('.description').val();
-    var time = $(this).parent().attr('id');
+saveTask.setItem('todo',function(){
+  console.log('hey');
 })
 
 
 
+// $text.innerHTML
 
-
-// text.innerHTML = localStorage.getItem($text);
-// $text.textContent = text;
 // let val_serialized = JSON.stringify(val);
 // saveTask.click(function(){
-//    var value = $(this).siblings('.px-5').val();
+   
 //     var time = $(this).parent().attr('id'); // will be a changing value based on time
 // localStorage.setItem(time,value);
 // });
 
 
-
-
-// want to change time text with each  iteritation in hour div
-
-
-
-  // localStorage.setItem('todo', JSON.stringify($text));
-  // var textL = JSON.parse(localStorage.getItem('todo'));
-  // localStorage.setItem(textarea,'textarea');
-  // alert( event.currentTarget === this );
-  // console.log(textL);
-  // localStorage.setItem('');   -for when i get the cards done
-
-
-
-
-//what does each row have to do:
-//-Display current hour (9am 10am..)
-//-indicate if the row is past-present-future
-// display whatever entry is availablle for that time block
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
+// localStorage.setItem('');   -for when i get the cards done
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?

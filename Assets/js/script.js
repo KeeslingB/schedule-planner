195ad78db8a606061e15f1335a11e05db2e8c527
unknown <Breke64@gmail.com>
2023-10-24 22:57:02 -0400

    finishing up homework - will return tomrrow for screenchot and finishing touches

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,79 +1,102 @@
-var currentTime = dayjs().format(' h:mm:ss, dddd MMMM DD, YYYY');
-console.log(currentTime); // will be used to compare each block with.
+var currentTime = dayjs().format('h:mm:ss, dddd MMMM DD, YYYY');
+ // will be used to compare each block with.
 var $currentTime = $('p.currentDay');
 var $cards = $('div.row');   //class will change based on current time then will trigger respective css styles. //root?
-let $text = $('textarea.description');  //used for storing input into local storage.
-saveBtn = document.getElementsByClassName('fa-save');
+let $text = $('textarea.description');
 var $hour = $('div.hour');
 $currentTime.text(currentTime);
-
+// var save = document.querySelector('#save');
 let scheduleCards = $('.container-lg');
 var localStorage = $('px-5');
-var saveTask = document.getElementsByClassName('i.fas');
+var saveTask = document.getElementsByClassName('.fas');
 
-for (var i = 9; i < 13; i++) {
-  if (i < currentTime) {
-    am = 'past';
-  }
-  if (i == currentTime) {
-    am = 'present';
-  } else (am = 'future');
-  let newBlockAm = `
-<div id="hour-${i}am  root" class="row time-block ${am}">
-<div class="col-2 col-md-1 hour text-center py-3">${i}AM</div>
-<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  <i class="fas fa-save" aria-hidden="false"></i>
-</button>
-</div>`;
-  $hour.text(i);
-  scheduleCards.append(newBlockAm);
-  // console.log(newBlockAm);
-};
+console.log(currentTime);
 
-for (var i = 1; i < 6; i++) {
-  if (i < currentTime) {
-    pm = 'past';
-  }
-  if (i == currentTime) {
-    pm = 'present';
-  } else (pm = 'future');
-  let newBlockPm = `
-  <div id="hour-${i}pm  root" class="row time-block ${pm}">
-  <div class="col-2 col-md-1 hour text-center py-3">${i}PM</div>
-  <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-  <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-    <i class="fas fa-save" aria-hidden="false"></i>
-  </button>
-  </div>`;
-  scheduleCards.append(newBlockPm);
-  $hour.text(i);
-  // console.log(newBlockPm);
-};
+function updateHour (){
+  newDate = dayjs().hour();
+console.log(newDate);
+  $('.time-block').each(function(){
+var blockHour = parseInt($(this).attr('id').split('-')[1])
+if(blockHour < newDate){
+  $(this).addClass('past');
+} else if(blockHour === newDate){
+  $(this).removeClass('past');
+  $(this).addClass('present');
+} else {
+  $(this).removeClass('past')
+  $(this).removeClass('present');
+  $(this).addClass('future');
+}
+console.log(blockHour);
+  })
+}
 
-saveTask.setItem('todo',function(){
-  console.log('hey');
+updateHour();
+// setInterval(updateHour,15000)
+$('.saveBtn').on('click',function(){
+  var value = $(this).siblings('.description').val();
+  var time = $(this).parent().attr('id');
+  localStorage.setItem(time,value);
 })
 
+ $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+ $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+ $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+ $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+ $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+ $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+ $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+ $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+ $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
-// $text.innerHTML
-
-// let val_serialized = JSON.stringify(val);
-// saveTask.click(function(){
-   
-//     var time = $(this).parent().attr('id'); // will be a changing value based on time
-// localStorage.setItem(time,value);
-// });
 
 
-// localStorage.setItem('');   -for when i get the cards done
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
+// originally tried using jquery as seen below but encountered some issues ,got through some but one kept giving me a hard time so opted to keep most of the project in html. 
+
+// var am = "";
+
+// for (var i = 9; i < 13; i++) {
+//   if(newDate == i){
+//     am = 'present';
+//   }if (i > newDate){
+//     am = 'future';
+//   } else if (i < newDate){
+//     am = 'past';
+//   }
+//   let newBlockAm = `
+// <div id="hour-${i}am  root" class="row time-block ${am}">
+// <div class="col-2 col-md-1 hour text-center py-3">${i}AM</div>
+// <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+// <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//   <i id="save" class="fas fa-save" aria-hidden="false"></i>
+// </button>
+// </div>`;
+//   $hour.text(i);
+//   scheduleCards.append(newBlockAm);
+//   console.log(newBlockAm);
+// };
+
+// let pm = '';
+
+// for (var i = 1; i < 12; i++){
+//   // if(i == newDate){
+//   //   pm = 'present';
+//   // } if (i > newDate){
+//   //   pm = 'future';
+//   // } else if( i < newDate){
+//   //   pm = 'past';
+//   // }
+//   let newBlockPm = `
+//   <div id="hour-${i}pm  root" class="row time-block ${am}">
+//   <div class="col-2 col-md-1 hour text-center py-3">${i}PM</div>
+//   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+//   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+//     <i id="save" class="fas fa-save" aria-hidden="false"></i>
+//   </button>
+//   </div>`;
+//   scheduleCards.append(newBlockPm);
+//   $hour.text(i);
+//   console.log(newBlockPm);
+//   console.log(i);
+// };
